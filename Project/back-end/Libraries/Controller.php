6a2cb53f49eec26a6efd8b965c94c4d341bea927
Mouhcine Daali <daalim277@gmail.com>
2022-05-18 04:10:18 +0100

    modify the code in the controller file

--- Project/back-end/Libraries/Controller.php
+++ Project/back-end/Libraries/Controller.php
@@ -4,7 +4,7 @@
         // load model
         public function model($model) {
             // Require model file
-            require_once '../app/models/' . $model . '.php';
+            require_once '../models/' . $model . '.php';
             // Instantiate model class
             return new $model();
         }

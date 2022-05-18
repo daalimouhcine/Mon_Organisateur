02b7f9fda1b01ff54df7a7765ac28741dbf20615
Mouhcine Daali <daalim277@gmail.com>
2022-05-18 04:10:50 +0100

    modify database file

--- Project/back-end/Libraries/Database.php
+++ Project/back-end/Libraries/Database.php
@@ -1,73 +1,78 @@
 <?php
 
-    class Database {
-        private $host = 'localhost';
-        private $user = "root";
-        private $pass = "";
-        private $dbname = "mon_organisateur";
-        private $dbh;
-        private $error;
+    class Database{
+        private $host = "localhost";
+        private $db_name = "mon_organisateur";
+        private $username = "root";
+        private $password = "";
+
+        private $conn;
         private $stmt;
 
-        public function __construct() {
-            // Set DSN
-            $dsn = 'mysql:host=' . $this->host . ':dbname=' . $this->dbname;
-            // Set options
-            $options = array(
-                PDO::ATTR_PERSISTENT => TRUE,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            );
-            // Create a new PDO instance
-            try {
-                $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-                
-            } catch(PDOException $e) {
-                $this->error = $e->getMessage();
+        
+        public function __construct(){
+            // Set Connection to Database
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
             }
+
+            return $this->conn;
         }
 
         public function query($query) {
-            $this->stmt = $this->dbh->prepare($query);
+            // Prepare Statement
+            $this->stmt = $this->conn->prepare($query);
         }
 
         public function bind($param, $value, $type = null) {
-            if(is_null($type)) {
-                switch (true) {
-                    case is_int($value):
-                        $type = PDO::PARAM_INT;
-                        break;
-                    case is_bool($value):
-                        $type = PDO::PARAM_BOOL;
-                        break;
-                    case is_null($value):
-                        $type = PDO::PARAM_NULL;
-                        break;
-                    default:
-                        $type = PDO::PARAM_STR;
-                }
+            // Check the type of the value
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
             }
+
+            $this->stmt->bindParam($param, $value, $type);
         }
 
         public function execute() {
+            // Execute the prepared statement
             return $this->stmt->execute();
         }
 
         public function resultSet() {
+            // Execute the prepared statement
             $this->execute();
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+            // Get the results
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
         }
 
         public function single() {
+            // Execute the prepared statement
             $this->execute();
-            return $this->stmt->fetch(PDO::FETCH_ASSOC);
+            // Get one result
+            return $this->stmt->fetch(PDO::FETCH_OBJ);
         }
 
         public function rowCount() {
+            // Execute the prepared statement
+            $this->execute();
+            // Get the row count
             return $this->stmt->rowCount();
         }
-
-        public function lastInsertId() {
-            return $this->dbh->lastInsertId();
-        }
-        
     }
+
+
+?>

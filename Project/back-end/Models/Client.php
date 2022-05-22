c4d009e69dfb26733312d18b0435b701bb20778b
Mouhcine Daali <daalim277@gmail.com>
2022-05-22 23:57:58 +0100

    modify client model

--- Project/back-end/Models/Client.php
+++ Project/back-end/Models/Client.php
@@ -8,18 +8,16 @@
 
         public function addClient($data) {
             // Prepare Query
-            $this->db->query('INSERT INTO clients (nom, prenom, image_profile, gender, nom_utilisateur, email, mot_de_passe, telephone, date_naissance, role) VALUES (:nom, :prenom, :image_profile, :gender, :nom_utilisateur, :email, :mot_de_passe, :telephone, :date_naissance, :role)');
+            $this->db->query('INSERT INTO clients (nom, prenom, image_profile, nom_utilisateur, email, mot_de_passe, telephone, role) VALUES (:nom, :prenom, :image_profile, :nom_utilisateur, :email, :mot_de_passe, :telephone, :role)');
             
             // Bind Data
             $this->db->bind(':nom', $data->nom);
             $this->db->bind(':prenom', $data->prenom);
             $this->db->bind(':image_profile', $data->image_profile);
-            $this->db->bind(':gender', $data->gender);
             $this->db->bind(':nom_utilisateur', $data->nom_utilisateur);
             $this->db->bind(':email', $data->email);
             $this->db->bind(':mot_de_passe', $data->mot_de_passe);
             $this->db->bind(':telephone', $data->telephone);
-            $this->db->bind(':date_naissance', $data->date_naissance);
             $this->db->bind(':role', $data->role);
             
             // Execute Query
@@ -75,18 +73,16 @@
 
         public function updateClient($data, $id) {
             // Prepare Query
-            $this->db->query('UPDATE clients SET nom = :nom, prenom = :prenom, image_profile = :image_profile, gender = :gender, nom_utilisateur = :nom_utilisateur, email = :email, mot_de_passe = :mot_de_passe, telephone = :telephone, date_naissance = :date_naissance, role = :role WHERE id = :id');
+            $this->db->query('UPDATE clients SET nom = :nom, prenom = :prenom, image_profile = :image_profile, nom_utilisateur = :nom_utilisateur, email = :email, mot_de_passe = :mot_de_passe, telephone = :telephone, role = :role WHERE id = :id');
             
             // Bind Data
             $this->db->bind(':nom', $data->nom);
             $this->db->bind(':prenom', $data->prenom);
             $this->db->bind(':image_profile', $data->image_profile);
-            $this->db->bind(':gender', $data->gender);
             $this->db->bind(':nom_utilisateur', $data->nom_utilisateur);
             $this->db->bind(':email', $data->email);
             $this->db->bind(':mot_de_passe', $data->mot_de_passe);
             $this->db->bind(':telephone', $data->telephone);
-            $this->db->bind(':date_naissance', $data->date_naissance);
             $this->db->bind(':role', $data->role);
             $this->db->bind(':id', $id);
 

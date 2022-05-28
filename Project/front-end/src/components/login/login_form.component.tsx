32cfa73776509ea8476e161ed186ef80992c2331
Mouhcine Daali <daalim277@gmail.com>
2022-05-28 23:58:43 +0100

    compleet the login component

--- Project/front-end/src/components/login/login_form.component.tsx
+++ Project/front-end/src/components/login/login_form.component.tsx
@@ -1,56 +1,115 @@
+import axios from "axios";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import type { LoginInputs, LoginMessage } from "../../models";
 
 import "./login.component.css";
 
-
 const LoginForm: React.FC = () => {
+  const [loginMessage, setLoginMessage] = useState<LoginMessage>({
+    message: "",
+    type: "",
+  });
+  // const [shouldNavigate, setShouldNavigate] = useState<boolean>(false);
 
-    return (
+  const {
+    register, // register the input
+    handleSubmit, // <- needed to bind the form
+    formState: { errors }, // to get the form state
+  } = useForm<LoginInputs>();
 
-        <div id='bg_form_style' className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0">
-            <div id='bg_form_img' className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center">
-                <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
-            </div>
-            <div className="w-full py-6 z-20">
-                <h1 className="mb-6">
-                    logo
-                </h1>
-                <div className="py-6 space-x-2">
-                    <span className="w-10 h-10 items-center justify-center inline-flex rounded-full font-bold text-lg border-2 border-white">f</span>
-                    <span className="w-10 h-10 items-center justify-center inline-flex rounded-full font-bold text-lg border-2 border-white">G+</span>
-                    <span className="w-10 h-10 items-center justify-center inline-flex rounded-full font-bold text-lg border-2 border-white">in</span>
-                </div>
-                <p className="text-gray-100">
-                    or use email your account
-                </p>
-                <form action="" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
-                    <div className="pb-2 pt-4">
-                        <input type="email" name="email" id="email" placeholder="Email" className="block w-full p-4 text-lg rounded-sm bg-black" />
-                    </div>
-                    <div className="pb-2 pt-4">
-                        <input className="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password" id="password" placeholder="Password" />
-                    </div>
-                    <div className="text-right text-gray-400 hover:underline hover:text-gray-100">
-                        <p>Forgot your password?</p>
-                    </div>
-                    <div className="px-4 pb-2 pt-4">
-                        <button className="uppercase block w-full p-4 text-lg rounded-full bg-indigo-500 hover:bg-indigo-600 focus:outline-none">sign in</button>
-                    </div>
+  const navigate = useNavigate();
 
-                    <div className="p-4 text-center right-0 left-0 flex justify-center space-x-4 mt-16 lg:hidden ">
-                        <a href="#_">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
-                        </a>
-                        <a href="#_">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
-                        </a>
-                        <a href="#_">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
-                        </a>
-                    </div>
-                </form>
-            </div>
+  const requestLogin: SubmitHandler<LoginInputs> = async (data) => {
+    await axios.post("http://localhost/mon_organisateur/clients/login", data)
+    .then(res => {
+      if(res.data.error) {
+        setLoginMessage({ message: res.data.error, type: "error" });
+      } else {
+        localStorage.setItem("token", res.data.token);
+        navigate("/");
+      }
+    });
+  };
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    requestLogin(data);
+  };
+
+  return (
+    <div className="flex md:w-1/2 justify-center pb-10 items-center bg-white mt-12">
+      <form className="bg-white mx-5" onSubmit={handleSubmit(onSubmit)}>
+        <h3
+          className={`font-bold text-xl mb-7 ${
+            loginMessage.type === "error" ? "text-red-500" : "text-green-500"
+          }`}
+        >
+          {loginMessage.message}
+        </h3>
+        <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
+        <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
+        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+            />
+          </svg>
+          <input
+            className="pl-2 outline-none border-none"
+            placeholder="Email Address"
+            type="email"
+            {...register("email", { required: "Email est obligatoire" })}
+          />
         </div>
-    );
-}
+        <p className="text-red-500">{errors.email?.message}</p>
+        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            className="pl-2 outline-none border-none"
+            type="text"
+            id=""
+            placeholder="********"
+            {...register("mot_de_passe", {
+              required: "Mot de passe est obligatoire",
+            })}
+          />
+        </div>
+        <p className="text-red-500">{errors.mot_de_passe?.message}</p>
+        <input
+          type="submit"
+          className="block w-full bg-indigo-600 mt-4 py-2 rounded-md text-white font-semibold mb-2 cursor-pointer"
+          value="S'inscrire"
+        />
+        <span className="text-sm ml-2">
+          Don't have account?{" "}
+          <NavLink to="/register" className="text-blue-500">
+            Register
+          </NavLink>
+        </span>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;

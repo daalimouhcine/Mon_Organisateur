894f045b2518b56fa3adb453e7a0edb7ddd191ba
Mouhcine Daali <daalim277@gmail.com>
2022-05-30 23:41:09 +0100

    create a register page to forward to register forms

---
+++ Project/front-end/src/components/register/register.component.tsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom";
+
+const Register: React.FC = () => {
+  return (
+    <div className="h-screen md:flex">
+      <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+        <div>
+          <h1 className="text-white font-bold text-4xl font-sans">
+            Organisateur
+          </h1>
+          <p className="text-white mt-1">
+            The most popular peer to peer lending at SEA
+          </p>
+          <NavLink
+            to="/register/organisateur"
+            className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+          >
+            Organisateur Register Here
+          </NavLink>
+        </div>
+        <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+      </div>
+      <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+        <div>
+          <h1 className="text-white font-bold text-4xl font-sans">Client</h1>
+          <p className="text-white mt-1">
+            The most popular peer to peer lending at SEA
+          </p>
+          <NavLink
+            to="/register/client"
+            className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+          >
+            Client Register Here
+          </NavLink>
+        </div>
+        <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;

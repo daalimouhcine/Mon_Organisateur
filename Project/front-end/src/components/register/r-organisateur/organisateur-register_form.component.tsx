a5ebc8d59fa5f53f83e4f3a0543c908471edd047
Mouhcine Daali <daalim277@gmail.com>
2022-06-01 18:56:43 +0100

    complet the multi step form logic

--- Project/front-end/src/components/register/r-organisateur/organisateur-register_form.component.tsx
+++ Project/front-end/src/components/register/r-organisateur/organisateur-register_form.component.tsx
@@ -1,176 +1,191 @@
-import axios from "axios";
-import { useState } from 'react';
-import { NavLink, useNavigate } from "react-router-dom";
-import { useForm, SubmitHandler } from "react-hook-form";
-import type { RegisterInputs, RegisterMessage } from "../../../models";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { OrganiserRegisterInputs } from "../../../models";
 
-import { PhoneIcon } from "../../icons/contact/phone-icon";
+const MAX_STEPS = 3;
 
-const OrganisateurRegisterForm: React.FC = () => {
-
-  const [registerMessage, setRegisterMessage] = useState<RegisterMessage>({
-    message: "",
-    type: "",
-  });
-
-  
-  const navigate = useNavigate();
-
+const OrganisateurRegisterForm = () => {
   const {
-    register, // register the input
-    handleSubmit, // <- needed to bind the form
-    // watch, // to watch the value of a specific input
-    formState: { errors }, // to get the form state
-  } = useForm<RegisterInputs>();
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<OrganiserRegisterInputs>({ mode: "all" });
 
-  const onSubmit: SubmitHandler<RegisterInputs> = (data) => {
-    axios.post("http://localhost/mon_organisateur/clients/register", data).then((res) => {
-      if(!res.data.error) {
-        setRegisterMessage({message:'Votre compte a été créé avec succès', type:'success'});
-        
-        setTimeout(() => {
-          navigate("/login");
-        }, 1000);
-        
-      } else {
-        setRegisterMessage({message: res.data.error, type:'error'});
-      }
-      console.log(res);
-    });
+  const [formStep, setFormStep] = React.useState(0);
+
+  const handleStepCompletion = () => {
+    isValid && setFormStep((cur) => cur + 1);
   };
 
-
-  // console.log(watch()); // Src: https://react-hook-form.com/get-started/ , watch input value by passing the name of it
-  // console.log(errors);
+  const handleFormCompletion = (values: any) => {
+    window.alert(JSON.stringify(values, null, 2));
+    setFormStep((cur) => cur + 1);
+  };
 
   return (
-    <div className="flex md:w-1/2 justify-center pb-10 items-center bg-white mt-12">
-      <form className="bg-white mx-5" onSubmit={handleSubmit(onSubmit)}>
-      <h3 className={`font-bold text-xl mb-7 ${registerMessage.type === 'error' ? 'text-red-500' : 'text-green-500'}`}>{registerMessage.message}</h3>
-        <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
-        <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <input
-            className="pl-2 w-1/2 outline-none border-none"
-            type="text"
-            id=""
-            placeholder="Nom"
-            {...register("nom", { required: "Nom est obligatoire" })}
-          />
-          <input
-            className="pl-2 w-1/2 border-l border-l-slate-300 outline-none "
-            id=""
-            placeholder="Prenom"
-            {...register("prenom", { required: "prenom est obligatoire" })}
-          />
-        </div>
-        <p className="text-red-500">
-          {errors.nom?.message} {(errors.nom?.message && errors.prenom?.message) && " et "} {errors.prenom?.message}
+    <div className=" bg-green-800 w-1/2 h-fit">
+      <div className="mx-auto text-center my-10">
+        <h1 className="text-white text-5xl font-semibold">
+          Welcome to <span className="text-yellow-500">the Club</span>
+        </h1>
+        <p className="text-green-200 mt-2">
+          Become a new member in 3 easy steps
         </p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-            />
-          </svg>
-          <input
-            className="pl-2 outline-none border-none"
-            type="text"
-            id=""
-            placeholder="Nom d'utilisateur"
-            {...register("nom_utilisateur", { required: "Nom d'utilisateur est obligatoire" })}
-          />
+      </div>
+      <div className="max-w-xl w-full mb-24 rounded-lg shadow-2xl bg-white mx-auto overflow-hidden z-10">
+        {formStep < 3 && (
+          <div className="h-2 w-full bg-gray-200">
+            <div
+              style={{ width: `${((formStep + 1) / MAX_STEPS) * 100}%` }}
+              className="h-full bg-yellow-400"
+            ></div>
+          </div>
+        )}
+        <div className="px-16 py-10">
+          {formStep < 3 && (
+            <div
+              className={`flex ${
+                formStep === 0 ? "justify-end" : "justify-between"
+              } items-center mb-6 font-medium text-sm`}
+            >
+              {formStep > 0 && (
+                <button
+                  onClick={() => setFormStep((cur) => cur - 1)}
+                  className="flex items-center text-gray-400 hover:text-gray-500 focus:outline-none"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-5 mr-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 19l-7-7 7-7"
+                    />
+                  </svg>
+                  Previous
+                </button>
+              )}
+              <p className="">
+                Step {formStep + 1} of {MAX_STEPS}
+              </p>
+            </div>
+          )}
+          <form onSubmit={handleSubmit(handleFormCompletion)}>
+            {formStep >= 0 && (
+              <section className={formStep === 0 ? "block" : "hidden"}>
+                <h2 className="font-semibold text-3xl mb-8">
+                  Personal Information
+                </h2>
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="nom"
+                  className="text-input"
+                  {...register("nom", { required: "Please type your nom" })}
+                />
+                {errors.nom && (
+                  <p className="text-red-600 text-sm mt-2">
+                    {errors.nom.message}
+                  </p>
+                )}
+              </section>
+            )}
+            {formStep >= 1 && (
+              <section className={formStep === 1 ? "block" : "hidden"}>
+                <h2 className="font-semibold text-3xl mb-8">
+                  Billing Information
+                </h2>
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  className="text-input"
+                  {...register("adresse", {
+                    required: "adresse est obligatoire",
+                  })}
+                />
+                {errors.adresse && (
+                  <p className="text-red-600 text-sm mt-2">
+                    {errors.adresse.message}
+                  </p>
+                )}
+              </section>
+            )}
+            {formStep >= 2 && (
+              <section className={formStep === 2 ? "block" : "hidden"}>
+                <h2 className="font-semibold text-3xl mb-8">
+                  Legal Information
+                </h2>
+                <div className="block mt-6">
+                  <input
+                    className="p-3 text-green-600 rounded mr-3 border-2 border-gray-300 ring-0 focus:ring-0 focus:ring-offset-0 focus:border-0 cursor-pointer"
+                    type="email"
+                    {...register("email", {
+                      required: "email est obligatoire",
+                    })}
+                  />
+                  <span>
+                    I accept the{" "}
+                    <a className="text-blue-400 underline" href="/">
+                      Terms and Conditions
+                    </a>
+                    .
+                  </span>
+                  {errors.email && (
+                    <p className="text-red-600 text-sm mt-2">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+                <div className="block mt-6">
+                  <input
+                    className="p-3 text-green-600 rounded mr-3 border-2 border-gray-300 ring-0 focus:ring-0 focus:ring-offset-0 focus:border-0 cursor-pointer"
+                    type="checkbox"
+                    {...register("cin", { required: "cin est obligatoire" })}
+                  />
+                  <span>
+                    I accept the{" "}
+                    <a className="text-blue-400 underline" href="/">
+                      Privacy Policy
+                    </a>
+                    .
+                  </span>
+                  {errors.cin && (
+                    <p className="text-red-600 text-sm mt-2">
+                      {errors.cin.message}
+                    </p>
+                  )}
+                </div>
+              </section>
+            )}
+            {formStep >= 3 && (
+              <section>
+                <h2 className="font-semibold text-3xl mb-8">
+                  Thank you for signing up!
+                </h2>
+                <p>You can now log in with your new account</p>
+              </section>
+            )}
+            {formStep < 3 && (
+              <button
+                disabled={!isValid}
+                onClick={formStep === 2 ? undefined : handleStepCompletion}
+                type={formStep === 2 ? "submit" : "button"}
+                className="mt-6 bg-green-600 text-white rounded px-8 py-6 w-full disabled:bg-gray-400"
+              >
+                Next
+              </button>
+            )}
+            <pre>{JSON.stringify(watch(), null, 2)}</pre>
+          </form>
         </div>
-          <p className="text-red-500">{errors.nom_utilisateur?.message}</p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-            />
-          </svg>
-          <input
-            className="pl-2 outline-none border-none"
-            placeholder="Email Address"
-            type="email"
-            {...register("email", { required: 'Email est obligatoire' })}
-          />
-        </div>
-          <p className="text-red-500">{errors.email?.message}</p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <PhoneIcon className="w-4 text-gray-400" />
-          <input
-            className="pl-2 outline-none border-none"
-            type="text"
-            id=""
-            placeholder="Numero de telephone"
-            {...register("telephone", { required: 'Numero de telephone est obligatoir' })}
-          />
-        </div>
-          <p className="text-red-500">{errors.telephone?.message}</p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <input
-            className="pl-2 outline-none border-none"
-            type="password"
-            id=""
-            placeholder="********"
-            {...register("mot_de_passe", { required: "Mot de passe est obligatoire" })}
-          />
-        </div>
-          <p className="text-red-500">{errors.mot_de_passe?.message}</p>
-        <input
-          type="submit"
-          className="block w-full bg-indigo-600 mt-4 py-2 rounded-md text-white font-semibold mb-2 cursor-pointer"
-          value="S'inscrire"
-        />
-        <span className="text-sm ml-2">
-          Have an account?{" "}
-          <NavLink to="/login" className="text-blue-500">
-            Login
-          </NavLink>
-        </span>
-      </form>
+      </div>
     </div>
   );
 };

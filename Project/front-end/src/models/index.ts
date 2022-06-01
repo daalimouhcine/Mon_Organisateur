632baa631d5eaf55130d4859a88025d5e6379e67
Mouhcine Daali <daalim277@gmail.com>
2022-06-01 18:57:03 +0100

    add interface for the organisateur's input

--- Project/front-end/src/models/index.ts
+++ Project/front-end/src/models/index.ts
@@ -13,6 +13,23 @@ export interface RegisterInputs {
   mot_de_passe: string;
 }
 
+export interface OrganiserRegisterInputs {
+  nom: string;
+  prenom: string;
+  nom_utilisateur: string;
+  telephone: string;
+  email: string;
+  mot_de_passe: string;
+  nom_entreprise: string;
+  cin: string;
+  adresse: string;
+  logo: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  linkedin?: string;
+}
+
 export interface RegisterMessage {
   message: string;
   type: string;

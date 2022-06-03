4cf78280ff66d528879c7445db15e76c63272c99
Mouhcine Daali <daalim277@gmail.com>
2022-06-03 21:20:09 +0100

    modify the client register form component

--- Project/front-end/src/components/register/r-client/cilent-register_form.component.tsx
+++ Project/front-end/src/components/register/r-client/cilent-register_form.component.tsx
@@ -90,30 +90,6 @@ const ClientRegisterForm: React.FC = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-            />
-          </svg>
-          <input
-            className="pl-2 outline-none border-none"
-            type="text"
-            id=""
-            placeholder="Nom d'utilisateur"
-            {...register("nom_utilisateur", { required: "Nom d'utilisateur est obligatoire" })}
-          />
-        </div>
-          <p className="text-red-500">{errors.nom_utilisateur?.message}</p>
-        <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
               d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
             />
           </svg>

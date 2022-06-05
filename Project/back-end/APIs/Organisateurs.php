ba41fcb084c94e42f80d272f1ec436679a9596e8
Mouhcine Daali <daalim277@gmail.com>
2022-06-05 23:12:29 +0100

    compleet the organisateur controller

--- Project/back-end/APIs/Organisateurs.php
+++ Project/back-end/APIs/Organisateurs.php
@@ -1,13 +1,13 @@
 <?php 
 
-    // set headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json ; charset=utf-8');
-    header("Access-Control-Allow-Methods: *"); 
-    header("Access-Control-Max-Age: 600");
-    header("Access-Control-Allow-Headers: *");
+// set headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json ; charset=utf-8');
+header("Access-Control-Allow-Methods: *"); 
+header("Access-Control-Max-Age: 600");
+header("Access-Control-Allow-Headers: *");
 
-    class Organisateurs extends Controller {
+class Organisateurs extends Controller {
 
         public function __construct() {
             $this->organisateurModel = $this->model('Organisateur');
@@ -17,62 +17,41 @@
             // get data
             $data = json_decode(file_get_contents('php://input'));
 
-            // check if organizer already exists
-            
-            $org = $this->organisateurModel->getOrganisateurByEmail($data->email);
-
-            // return json_encode($org);
-            // echo $org;
-            // die($org);
-            // check if organizer already exists
-            if($this->organisateurModel->getOrganisateurByEmail($data->email)) {
-                echo json_encode(['error' => 'email already exists']);
+            // check if user already exists 
+            if($this->organisateurModel->getOrganisateurByCin($data->cin)) {
+                echo json_encode(
+                    array('error' => 'CIN already exists')
+                );
                 return;
-                
+
+            } else if($this->organisateurModel->getOrganisateurByNomEntreprise($data->nom_entreprise)) {
+                echo json_encode(
+                    array('error' => "Nom de l'entreprise already exists")
+                );
+                return;
+
             } else if($this->organisateurModel->getOrganisateurByTelephone($data->telephone)) {
                 echo json_encode(['error' => 'number already exists']);
                 return;
-            
+
+            } else if($this->organisateurModel->getOrganisateurByEmail($data->email)) {
+                echo json_encode(['error' => 'email already exists']);
+                return;
+        
             } else {
                 // hash password
                 $data->mot_de_passe = password_hash($data->mot_de_passe, PASSWORD_DEFAULT);
-                // add organizer
+                // add client
                 if($this->organisateurModel->addOrganisateur($data)) {
                     echo json_encode(['done' => 'Organisateur added']);
                 } else {
                     echo json_encode(['error' => 'Organisateur not added please try again']);
                 }
             }
-
-            // if($this->organisateurModel->getOrganisateurByEmail($data->email)) {
-            //     echo json_encode(['error' => 'email already exists']);
-            //     return;
-
-            // } else if($this->organisateurModel->getOrganisateurByTelephone($data->telephone)) {
-            //     echo json_encode(['error' => 'number already exists']);
-            //     return;
-
-            // } else if($this->organisateurModel->getOrganisateurByCin($data->cin)) {
-            //     echo json_encode(['error' => 'cin already exists']);
-            //     return;
-                
-            // } else if($this->organisateurModel->getOrganisateurByNomEntreprise($data->nom_entreprise)) {
-            //     echo json_encode([ 'error' => "Nom de l'entreprise already exists"]);
-            //     return;
-
-            // } else {
-            //     // hash password
-            //     $data->mot_de_passe = password_hash($data->mot_de_passe, PASSWORD_DEFAULT);
-            //     // add organizer
-            //     if($this->organisateurModel->addOrganisateur($data)) {
-            //         echo json_encode(['done' => $data]);
-            //     } else {
-            //         echo json_encode(['error' => 'Organisateur not added please try again']);
-            //     }
-
-            // }
-
-
         }
 
-    }
+
+
+
+
+}

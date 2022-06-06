d610573332196b43ca6f664ef3b181874a31b4e3
Mouhcine Daali <daalim277@gmail.com>
2022-06-06 14:24:18 +0100

    add config for firebase

--- Project/front-end/src/index.tsx
+++ Project/front-end/src/index.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app.component';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZgh2znpIjb78nP5MJmLKpTgURLdu2hyw",
+  authDomain: "mon-organisateur.firebaseapp.com",
+  projectId: "mon-organisateur",
+  storageBucket: "mon-organisateur.appspot.com",
+  messagingSenderId: "100954401641",
+  appId: "1:100954401641:web:014ad72445b49d076cabf8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,3 +30,5 @@ root.render(
   </React.StrictMode>
 );
 
+
+export default app;

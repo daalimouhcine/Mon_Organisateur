d8438746281f31f7647de41bf2891cc4897955df
Mouhcine Daali <daalim277@gmail.com>
2022-06-08 22:31:24 +0100

    create admin page file

---
+++ Project/front-end/src/pages/home/admin/admin.home.page.tsx
@@ -0,0 +1,12 @@
+import AdminHome from '../../../components/admin/admin.home.component';
+
+
+const AdminHomePage: React.FC = () => {
+
+    return (
+        <AdminHome />
+    );
+
+}
+
+export default AdminHomePage;

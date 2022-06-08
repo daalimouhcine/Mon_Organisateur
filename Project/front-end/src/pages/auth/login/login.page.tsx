a01c1d9f55c4d58d05a3e6779b0f5f72923bcb20
Mouhcine Daali <daalim277@gmail.com>
2022-06-08 22:34:25 +0100

    delete the call for login content compotnent

--- Project/front-end/src/pages/auth/login/login.page.tsx
+++ Project/front-end/src/pages/auth/login/login.page.tsx
@@ -1,12 +1,10 @@
 import LoginForm from "../../../components/login/login_form.component";
-import LoginSideContent from "../../../components/login/login_side_content.component";
 
 import "./login.page.css";
 
 const Login: React.FC = () => {
   return (
     <section className="h-screen md:flex">
-      <LoginSideContent />
       <LoginForm />
     </section>
   );

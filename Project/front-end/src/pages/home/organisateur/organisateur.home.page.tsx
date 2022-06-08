cd467b35bce7cad6c9c377bfe8034db5dbfc8acb
Mouhcine Daali <daalim277@gmail.com>
2022-06-08 22:35:23 +0100

    create organisateur page file

---
+++ Project/front-end/src/pages/home/organisateur/organisateur.home.page.tsx
@@ -0,0 +1,11 @@
+import OrganisateurHome from '../../../components/organisateur/organisateur.home.component';
+
+
+const OrganisateurHomePage: React.FC = () => {
+
+    return (
+        <OrganisateurHome />
+    );
+}
+
+export default OrganisateurHomePage;

58b27746733681a3de4b9937fd928d078483ff3c
Mouhcine Daali <daalim277@gmail.com>
2022-06-08 22:34:41 +0100

    modify client register page file

--- Project/front-end/src/pages/auth/register/r-client/client-register.page.tsx
+++ Project/front-end/src/pages/auth/register/r-client/client-register.page.tsx
@@ -1,6 +1,5 @@
 
-import ClientRegisterForm from "../../../../components/register/r-client/cilent-register_form.component";
-import ClientRegisterSideContent from "../../../../components/register/r-client/client-register_side_content.component";
+import ClientRegisterForm from "../../../../components/register/r-client/client-register_form.component";
 
 import "./client-register.page.css";
 
@@ -9,7 +8,6 @@ const ClientRegisterPage: React.FC = () => {
     return (
         <div className="h-screen md:flex">
             <ClientRegisterForm />
-            <ClientRegisterSideContent />
         </div>
     );
 }

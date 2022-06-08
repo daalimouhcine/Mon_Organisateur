ce6dd7c298bfd8b53854efcbb19bc76009d141bc
Mouhcine Daali <daalim277@gmail.com>
2022-06-08 11:37:21 +0100

    modify styling

--- Project/front-end/src/pages/contact/contact.page.tsx
+++ Project/front-end/src/pages/contact/contact.page.tsx
@@ -1,19 +1,42 @@
 import FaqSide from "../../components/contact/faq/faq_side.component";
 import FaqQuestions from "../../components/contact/faq/faq_questions.component";
 import ContactForm from "../../components/contact/contact_form/contact_form.component";
+import CSS from 'csstype';
 
 import "./contact.page.css";
+
+const contactStyle: CSS.Properties = {
+  backgroundImage: `url(${'https://cdn.devdojo.com/images/february2021/directory-bg.jpg'})`,
+  
+ }
 
 const Contact: React.FC = () => {
   return (
     <>
-      <section className="py-24 bg-white">
-        <div className="flex flex-col-reverse items-center px-10 mx-auto lg:flex-row max-w-7xl">
-            <FaqSide />
-            <FaqQuestions />
+      <section
+        className="relative"
+        style={contactStyle}
+      >
+        <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-30"></div>
+
+        <div className="relative z-20 px-4 py-24 mx-auto text-center text-white max-w-7xl lg:py-32">
+          <div className="flex flex-wrap text-white">
+            <div className="relative w-full px-4 mx-auto text-center xl:flex-grow-0 xl:flex-shrink-0">
+              <h1 className="mt-0 mb-2 text-4xl font-bold text-white sm:text-5xl lg:text-7xl">
+                Contact Directory
+              </h1>
+              <p className="mt-0 mb-4 text-base text-white sm:text-lg lg:text-xl">
+                Find the best places to eat, drink, and shop nearest to you.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
-        <ContactForm />
+      <div className="h-auto p-6 mx-auto space-y-3 overflow-hidden transform -translate-y-12 bg-white rounded-lg shadow-md  lg:max-w-6xl lg:flex-row lg:space-y-0 lg:space-x-3 flex flex-col-reverse items-center px-10 max-w-7xl">
+        <FaqSide />
+        <FaqQuestions />
+      </div>
+      <ContactForm />
     </>
   );
 };

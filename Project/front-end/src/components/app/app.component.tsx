55242ea2da3f6e33ecba1ca7f8590335b6a503b4
Mouhcine Daali <daalim277@gmail.com>
2022-06-09 23:48:20 +0100

    modify routing

--- Project/front-end/src/components/app/app.component.tsx
+++ Project/front-end/src/components/app/app.component.tsx
@@ -1,51 +1,66 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from '../header/header.component';
-import Home from '../../pages/home/home.page';
-import AdminHomePage from '../../pages/home/admin/admin.home.page';
-import About from '../../pages/about/about.page';
-import Contact from '../../pages/contact/contact.page';
-import Login from '../../pages/auth/login/login.page';
-import RegisterPage from '../../pages/auth/register/register.page';
-import ClientRegisterPage from '../../pages/auth/register/r-client/client-register.page';
-import OrganisateurRegisterPage from '../../pages/auth/register/r-organisateur/organisateur-register.page';
-import Footer from '../footer/footer.component';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "../header/header.component";
+import Home from "../../pages/home/home.page";
+import AdminHomePage from "../../pages/home/admin/admin.home.page";
+import OrganisateurHomePage from '../../pages/home/organisateur/organisateur.home.page';
+import About from "../../pages/about/about.page";
+import Contact from "../../pages/contact/contact.page";
+import Login from "../../pages/auth/login/login.page";
+import RegisterPage from "../../pages/auth/register/register.page";
+import ClientRegisterPage from "../../pages/auth/register/r-client/client-register.page";
+import OrganisateurRegisterPage from "../../pages/auth/register/r-organisateur/organisateur-register.page";
+import Footer from "../footer/footer.component";
+
+import "../../common/styles/index";
+import "./app.component.css";
 
 
-import '../../common/styles/index';
-import './app.component.css';
+const homePage = () => {
+  let user = JSON.parse(localStorage.getItem("user")!) || "";
+  
+  switch(user.role) {
+      case "admin":
+          return <AdminHomePage />;
 
-const App: React.FC = () => {
+      case "client":
+          return <Home />;
+          
+      case "organisateur":
+          return <OrganisateurHomePage />;
 
-    const homePage = () => {
-        let user = JSON.parse(localStorage.getItem('user')!) || '';
-        
-        if(user.role === 'admin') {
-            return <AdminHomePage />
-        } else if(user.role === 'client') {
-            return <Home />
-        } else if(user.role === 'organisateur') {
-            return <Home />
-        } else {
-            return <Home />
-        }
-    }
+      default:
+          return <Home />;
+  }
+};
 
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={homePage()} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/register/client" element={<ClientRegisterPage />} />
-                <Route path="/register/organisateur" element={<OrganisateurRegisterPage />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
-}
+const user = JSON.parse(localStorage.getItem("user")!) ? true : false;
+
+const App = () => {
+
+
+  return (
+    <Router>
+      {!user && <Header />}
+
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={homePage()} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register/client" element={<ClientRegisterPage />} />
+        <Route
+          path="/register/organisateur"
+          element={<OrganisateurRegisterPage />}
+        />
+      </Routes>
+      {!user && <Footer />}
+
+      {/* <Footer /> */}
+    </Router>
+  );
+};
 
 export default App;

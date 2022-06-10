6d1c016e46a410ebe43a8fcac174dcc829aaa1ca
Mouhcine Daali <daalim277@gmail.com>
2022-06-10 19:44:47 +0100

    complet the suspending and unsuspending and accepting organisateurs

--- Project/front-end/src/components/admin/organisateur.part.tsx
+++ Project/front-end/src/components/admin/organisateur.part.tsx
@@ -1,52 +1,46 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { OrganisateurData } from "../../models";
+import { Default_image } from "../../common/images";
 
-const organisateurs = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    department: "Optimization",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-    image:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  // More organisateurs...
-];
+
+const default_image: string = Default_image;
 
 const OrganisateurPart = () => {
+  const [organisateurs, setOrganisateurs] = useState<Array<OrganisateurData>>([]);
+  const [changeStatus, setChangeStatus] = useState<boolean>(false);
 
-    const [organisateurs, setOrganisateurs] = useState<Array | null>(null);
-    const response = async () => {
-        await axios.get('http://localhost/mon_organisateur/organisateurs/getAllOrganisateurs')
-        .then(data => {
-            setOrganisateurs(data.data);
+  const response = async () => {
+    await axios
+      .get(
+        "http://localhost/mon_organisateur/organisateurs/getAllOrganisateurs"
+      )
+      .then((data) => {
+        setOrganisateurs(data.data);
+      })
+  };
+
+  useEffect(() => {
+    response();
+  }, [changeStatus]);
+
+  const updateStatus = (id: number, status: number, index: number) => {
+    axios
+        .post(`http://localhost/mon_organisateur/organisateurs/changeOrganisateurStatus`, { status, id })
+        .then((res) => {
+            setChangeStatus(!changeStatus);
         })
-    }
-
-    useEffect( () => {
-        response();
-        console.log(organisateurs);
-    }, []);
-    
+}
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+          <h1 className="text-xl font-semibold text-gray-900">Organisateurs</h1>
           <p className="mt-2 text-sm text-gray-700">
             A list of all the users in your account including their name, title,
             email and role.
           </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Add user
-          </button>
         </div>
       </div>
       <div className="mt-8 flex flex-col">
@@ -89,45 +83,67 @@ const OrganisateurPart = () => {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {organisateurs.map((person) => (
+                  {organisateurs.map((person: OrganisateurData, index) => (
                     <tr key={person.email}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                         <div className="flex items-center">
                           <div className="h-10 w-10 flex-shrink-0">
                             <img
                               className="h-10 w-10 rounded-full"
-                              src={person.image}
+                              src={`${
+                                person.image_profile === "default.png"
+                                  ? default_image
+                                  : "https://firebasestorage.googleapis.com/v0/b/mon-organisateur.appspot.com/o/Orga%2person.image_profile?alt=media"
+                              }`}
                               alt=""
                             />
                           </div>
                           <div className="ml-4">
                             <div className="font-medium text-gray-900">
-                              {person.name}
+                              {person.nom + " " + person.prenom}
                             </div>
                             <div className="text-gray-500">{person.email}</div>
                           </div>
                         </div>
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <div className="text-gray-900">{person.title}</div>
-                        <div className="text-gray-500">{person.department}</div>
+                        <div className="text-gray-900">
+                          {person.nom_entreprise}
+                        </div>
+                        <div className="text-gray-500">{person.adresse}</div>
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Active
+                        <span
+                          className={`inline-flex rounded-full px-2 text-xs font-semibold leading-5 ${
+                            person.status === 0
+                              ? "bg-yellow-100 text-yellow-500"
+                              : person.status === -1
+                              ? "bg-red-100 text-red-500"
+                              : "bg-green-100 text-green-500"
+                          } `}
+                        >
+                          {person.status === 0
+                            ? "Pending"
+                            : person.status === -1
+                            ? "Rejected"
+                            : "Approved"}
                         </span>
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         {person.role}
                       </td>
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="_#"
+                        <button
+                          onClick={() => {updateStatus(person.id, person.status, index)}}
                           className="text-indigo-600 hover:text-indigo-900"
                         >
-                          Edit
-                          <span className="sr-only">, {person.name}</span>
-                        </a>
+                          {person.status === 0
+                            ? "Accept"
+                            : person.status === -1
+                            ? "unsuspend"
+                            : "Suspend"}
+                          <span className="sr-only">, {person.nom}</span>
+                        </button>
                       </td>
                     </tr>
                   ))}

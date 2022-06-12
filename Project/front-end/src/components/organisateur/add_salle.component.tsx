a644e404661c2987fbf218a827276a9af89329b6
Mouhcine Daali <daalim277@gmail.com>
2022-06-12 05:02:56 +0100

    compleet add salle pop up

--- Project/front-end/src/components/organisateur/add_salle.component.tsx
+++ Project/front-end/src/components/organisateur/add_salle.component.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { SalleDetail, Type } from 'src/models';
+import { SalleDetail, Type, OrganisateurData } from "src/models";
+import useLocalStorage from "../../common/hooks/useLocaleStorage";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 import "./add_salle.component.css";
-
-
 
 const AddSalle = () => {
   const [types, setTypes] = useState<Array<Type>>();
@@ -13,7 +14,7 @@ const AddSalle = () => {
     await axios
       .get("http://localhost/mon_organisateur/salles/getTypes")
       .then((data) => {
-          console.log(data.data);
+        console.log(data.data);
         setTypes(data.data);
       });
   };
@@ -25,46 +26,73 @@ const AddSalle = () => {
     register, // register the input
     handleSubmit, // <- needed to bind the form
     // watch, // to watch the value of a specific input
+    reset, // to reset the form
     formState: { errors }, // to get the form state
   } = useForm<any>();
-  
-  const onSubmit: SubmitHandler<SalleDetail> = (data) => {
-    console.log(data);
-    // axios
-    //   .post("http://localhost/mon_organisateur/clients/register", data)
-    //   .then((res) => {
-    //     if (!res.data.error) {
-    //       setRegisterMessage({
-    //         message: "Votre compte a été créé avec succès",
-    //         type: "success",
-    //       });
 
-    //       MySwal.fire(
-    //         "Vous avez créé un compte !",
-    //         "accéder à votre compte",
-    //         "success"
-    //       ).then(() => {
-    //         return setTimeout(() => {
-    //           navigate("/login");
-    //         }, 1000);
-    //       });
-    //     } else {
-    //       setRegisterMessage({ message: res.data.error, type: "error" });
-    //     }
-    //     console.log(res);
-    //   });
+  const [user] = useLocalStorage<OrganisateurData>("user");
+  const MySwal = withReactContent(Swal);
+
+  const onSubmit: SubmitHandler<SalleDetail> = (data) => {
+    let dataInput: any = { ...data };
+
+    // change name of the image to start with org_ and add random number to avoid duplicate name and add extension
+    let image = dataInput.images[0];
+    let imageName: string = image.name;
+    let imageExtension = imageName.split(".").pop();
+    let newImageName = `salle_${Math.floor(
+      Math.random() * 100000
+    )}.${imageExtension}`;
+
+    let salleData = {
+      ...data,
+      organisateur_id: user.id,
+      images: newImageName,
+    };
+    console.log(salleData);
+    axios
+      .post("http://localhost/mon_organisateur/salles/addSalle", salleData)
+      .then((res) => {
+        if (res.data) {
+          handleCloseForm();
+          MySwal.fire(
+            "Vous avez ajouter un salle !",
+            "Vous pouvez editer la salle dans la liste des salles",
+            "success"
+          ).then(() => {
+            setOpenForm(false);
+            return setTimeout(() => {
+              // navigate("/login");
+            }, 1000);
+          });
+        }
+        console.log(res);
+      });
   };
 
+  const [openForm, setOpenForm] = useState(false);
+
+  const handleCloseForm = () => {
+    // reset the form
+    openForm && reset();
+    setOpenForm(!openForm);
+  }
+
   return (
-    <div className="flex align-middle justify-center p-3 rounded-md">
-      <input id="button" type="checkbox" />
-      <label
-        htmlFor="button"
+    <div
+      className={`flex align-middle justify-center p-3 rounded-md ${
+        openForm && "openForm"
+      }`}
+    >
+      {/* <input id="button" type="checkbox" /> */}
+      <button
+        onClick={() => handleCloseForm()}
+        type="button"
         id="label_button"
         className="inline-flex items-center justify-center cursor-pointer rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
       >
         Add Salle
-      </label>
+      </button>
       <div className="modal">
         <form
           action=""
@@ -72,7 +100,7 @@ const AddSalle = () => {
           onSubmit={handleSubmit(onSubmit)}
         >
           <div className="user-box">
-          {/* <svg/> */}
+            {/* <svg/> */}
             <input
               className="pl-2 w-full outline-none border-none"
               placeholder="Ville"
@@ -97,7 +125,9 @@ const AddSalle = () => {
               className="pl-2 w-full outline-none border-none"
               placeholder="Nombre de places"
               type="number"
-              {...register("nombre_places", { required: "Nombre de places est obligatoire" })}
+              {...register("nombre_places", {
+                required: "Nombre de places est obligatoire",
+              })}
             />
           </div>
           <p className="text-red-500">{errors.nombre_places?.message}</p>
@@ -114,17 +144,65 @@ const AddSalle = () => {
           <div className="flex items-center border-2 py-2 px-3 rounded-md mt-4">
             {/* <svg/> */}
             <select {...register("type_id")}>
-                {types?.map((type: Type) => (
-                    <option key={type.id} value={type.id}>{type.nom}</option>
-                ))}
+              {types?.map((type: Type) => (
+                <option key={type.id} value={type.id}>
+                  {type.nom}
+                </option>
+              ))}
             </select>
-            </div>
-            <p className="text-red-500">{errors.prix?.message}</p>
-            <textarea
+          </div>
+          <p className="text-red-500">{errors.prix?.message}</p>
+          <textarea
             placeholder="Description"
-            {...register("description", { required: "Description est obligatoire" })}
+            {...register("description", {
+              required: "Description est obligatoire",
+            })}
+          />
+          <p className="text-red-500">{errors.description?.message}</p>
+          <div className="mt-6 flex flex-col">
+            <label
+              htmlFor="fileInput"
+              // type="button"
+              className="cursor-pointer w-fit mx-auto inline-flex justify-between items-center focus:outline-none border py-2 px-4 rounded-lg shadow-sm text-left text-gray-600 bg-white hover:bg-gray-100 font-medium"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="inline-flex flex-shrink-0 w-6 h-6 -mt-1 mr-1"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <rect x="0" y="0" width="24" height="24" stroke="none"></rect>
+                <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+                <circle cx="12" cy="13" r="3" />
+              </svg>
+              Browse Photo
+            </label>
+
+            <div className="mx-auto w-48 text-gray-500 text-xs text-center mt-1">
+              Click to add profile picture
+            </div>
+
+            <input
+              id="fileInput"
+              accept="image/*"
+              className="hidden"
+              type="file"
+              {...register("images", {
+                required: "images est obligatoire",
+              })}
             />
-            <p className="text-red-500">{errors.description?.message}</p>
+          </div>
+          <p className="text-red-500">{errors.images?.message}</p>
+
+          <input
+            type="submit"
+            className="block w-full bg-indigo-600 mt-4 py-2 rounded-md text-white font-semibold mb-2 cursor-pointer"
+            value="Ajouter"
+          />
         </form>
       </div>
     </div>

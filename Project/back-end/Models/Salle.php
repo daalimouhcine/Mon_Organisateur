dd5bd5d13b7055822d8a4cc90ad2d74276850fad
Mouhcine Daali <daalim277@gmail.com>
2022-06-12 20:54:47 +0100

    add method to delete salle

--- Project/back-end/Models/Salle.php
+++ Project/back-end/Models/Salle.php
@@ -17,8 +17,9 @@
         }
 
         public function addSalle($data) {
-            $this->db->query('INSERT INTO salles (organisateur_id, ville, address, nombre_places, type_id, description, images, prix) VALUES (:organisateur_id, :ville, :address, :nombre_places, :type_id, :description, :images, :prix)');
+            $this->db->query('INSERT INTO salles (organisateur_id, titre, ville, address, nombre_places, type_id, description, images, prix) VALUES (:organisateur_id, :titre, :ville, :address, :nombre_places, :type_id, :description, :images, :prix)');
             $this->db->bind(':organisateur_id', $data->organisateur_id);
+            $this->db->bind(':titre', $data->titre);
             $this->db->bind(':ville', $data->ville);
             $this->db->bind(':address', $data->address);
             $this->db->bind(':type_id', $data->type_id);
@@ -27,7 +28,22 @@
             $this->db->bind(':images', $data->images);
             $this->db->bind(':prix', $data->prix);
             return $this->db->execute();
+        }
 
+        public function getSalles() {
+            $this->db->query('SELECT salles.*,organisateurs.nom_entreprise, organisateurs.nom, organisateurs.prenom, organisateurs.image_profile, organisateurs.telephone, organisateurs.email, types.nom AS type FROM salles INNER JOIN organisateurs ON salles.organisateur_id = organisateurs.id INNER JOIN types ON salles.type_id = types.id');
+            $salles = $this->db->execute();
+            if($salles) {
+                return $this->db->resultSet();
+            } else {
+                return false;
+            }
+        }
+
+        public function deleteSalle($id) {
+            $this->db->query('DELETE FROM salles WHERE id = :id');
+            $this->db->bind(':id', $id);
+            return $this->db->execute();
         }
 
 

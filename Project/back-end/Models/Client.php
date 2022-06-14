5ee3e065e20758471fe76b1acfa68eed5ad36716
Mouhcine Daali <daalim277@gmail.com>
2022-06-14 03:10:37 +0100

    delete the profile image

--- Project/back-end/Models/Client.php
+++ Project/back-end/Models/Client.php
@@ -8,12 +8,11 @@
 
         public function addClient($data) {
             // Prepare Query
-            $this->db->query('INSERT INTO clients (nom, prenom, image_profile, email, mot_de_passe, telephone, role) VALUES (:nom, :prenom, :image_profile, :email, :mot_de_passe, :telephone, :role)');
+            $this->db->query('INSERT INTO clients (nom, prenom, email, mot_de_passe, telephone, role) VALUES (:nom, :prenom, :email, :mot_de_passe, :telephone, :role)');
             
             // Bind Data
             $this->db->bind(':nom', $data->nom);
             $this->db->bind(':prenom', $data->prenom);
-            property_exists($data, 'image_profile') ? $this->db->bind(':image_profile', $data->image_profile) : $this->db->bind(':image_profile', 'default.png');
             $this->db->bind(':email', $data->email);
             $this->db->bind(':mot_de_passe', $data->mot_de_passe);
             $this->db->bind(':telephone', $data->telephone);
@@ -72,12 +71,11 @@
 
         public function updateClient($data, $id) {
             // Prepare Query
-            $this->db->query('UPDATE clients SET nom = :nom, prenom = :prenom, image_profile = :image_profile, nom_utilisateur = :nom_utilisateur, email = :email, mot_de_passe = :mot_de_passe, telephone = :telephone, role = :role WHERE id = :id');
+            $this->db->query('UPDATE clients SET nom = :nom, prenom = :prenom, nom_utilisateur = :nom_utilisateur, email = :email, mot_de_passe = :mot_de_passe, telephone = :telephone, role = :role WHERE id = :id');
             
             // Bind Data
             $this->db->bind(':nom', $data->nom);
             $this->db->bind(':prenom', $data->prenom);
-            $this->db->bind(':image_profile', $data->image_profile);
             $this->db->bind(':nom_utilisateur', $data->nom_utilisateur);
             $this->db->bind(':email', $data->email);
             $this->db->bind(':mot_de_passe', $data->mot_de_passe);

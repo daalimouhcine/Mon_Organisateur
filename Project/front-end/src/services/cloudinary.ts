9be4e08e5ad4420cdb40c2f5a36124b52a26060d
Mouhcine Daali <daalim277@gmail.com>
2022-06-16 23:59:09 +0100

    add data to cloudinary file

--- Project/front-end/src/services/cloudinary.ts
+++ Project/front-end/src/services/cloudinary.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
 export const getSignature = async () => {
-    const reaponse = await axios.get('http://localhost/mon_organisateur/Cloudinary/getSignature');
-    return reaponse.data;
+    const response = await axios.get('http://localhost/mon_organisateur/Cloudinary/getSignature');
+    return response;
 }
+
+export const cloudinaryData = {
+    cloudName: "dbzzjmggw",
+    apiKey: "162644775298171",
+    folder: "MonOrganisateur"
+};

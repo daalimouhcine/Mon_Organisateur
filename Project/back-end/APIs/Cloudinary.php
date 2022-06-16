1ae500e467e4daac631073d7a5de20593664d74b
Mouhcine Daali <daalim277@gmail.com>
2022-06-16 23:58:29 +0100

    add headers

--- Project/back-end/APIs/Cloudinary.php
+++ Project/back-end/APIs/Cloudinary.php
@@ -1,5 +1,12 @@
 <?php
 
+// set headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json ; charset=utf-8');
+header("Access-Control-Allow-Methods: *"); 
+header("Access-Control-Max-Age: 600");
+header("Access-Control-Allow-Headers: *");
+
 class Cloudinary extends Controller {
     public function getSignature()
     {

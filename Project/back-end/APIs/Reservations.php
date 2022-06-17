1d7d49e334e60cc8685c666608ee20a129cf38e8
Mouhcine Daali <daalim277@gmail.com>
2022-06-17 22:15:10 +0100

    rename file

---
+++ Project/back-end/APIs/Reservations.php
@@ -0,0 +1,32 @@
+<?php
+
+    // set headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json ; charset=utf-8');
+    header("Access-Control-Allow-Methods: *"); 
+    header("Access-Control-Max-Age: 600");
+    header("Access-Control-Allow-Headers: *");
+
+    
+    class Reservations extends Controller {
+
+        public function __construct() {
+            $this->reservationModel = $this->model('Reservation');
+        }
+
+        public function reserveSalle() {
+            $reservationData = json_decode(file_get_contents('php://input'));
+            $response = $this->reservationModel->reserveSalle($reservationData);
+            if($response) {
+                echo json_encode('done');
+            } else {
+                echo json_encode('error');
+            }
+        }
+
+        public function getReservationDates() {
+            $salleId = json_decode(file_get_contents('php://input'));
+            $dates = $this->reservationModel->getReservationDates($salleId);
+            echo json_encode($dates);
+       }
+    }

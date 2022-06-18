21e23bbff2381ef4bb51cde2ad5e8cf7efc0f335
Mouhcine Daali <daalim277@gmail.com>
2022-06-18 21:19:33 +0100

    add more config to store image and get it from cloudinary

--- Project/front-end/src/services/cloudinary.ts
+++ Project/front-end/src/services/cloudinary.ts
@@ -5,8 +5,23 @@ export const getSignature = async () => {
     return response;
 }
 
-export const cloudinaryData = {
+export const cloudinaryInfo = {
     cloudName: "dbzzjmggw",
     apiKey: "162644775298171",
     folder: "MonOrganisateur"
 };
+
+
+
+export function toFormData(data: any) {
+    const formData = new FormData();
+    Object.entries(data).forEach(([key, val]) => {
+        formData.append(key, val as any);
+    })
+    return formData;
+}
+
+
+export function getCloudinaryImgUrl(publicId: any) {
+    return `https://res.cloudinary.com/${cloudinaryInfo.cloudName}/image/upload/v1654771394/${publicId}.jpg`
+}

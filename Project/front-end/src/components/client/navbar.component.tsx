fcd52147826210b1f54f2140449f5ffa568e660e
Mouhcine Daali <daalim277@gmail.com>
2022-06-19 23:57:48 +0100

    modify navbar

--- Project/front-end/src/components/client/navbar.component.tsx
+++ Project/front-end/src/components/client/navbar.component.tsx
@@ -1,38 +1,83 @@
-import { useState } from 'react';
-import './navbar.component.css';
+import { CalendarIcon, LogoutIcon, HomeIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+
+import { NavLink, useNavigate } from "react-router-dom";
+import { logout } from "src/services/logout";
+
+import "./navbar.component.css";
 
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const openMenu = (e: any) => {
-        setIsOpen(!isOpen);
-      };
+  const navigate = useNavigate();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = (e: any) => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       <nav className="nav">
-        <div className={`burger ${isOpen && 'burger--active'}`} onClick={(e) => {openMenu(e)}}>
+        <div
+          className={`burger ${isOpen && "burger--active"}`}
+          onClick={(e) => {
+            openMenu(e);
+          }}
+        >
           <div className="burger__patty" />
         </div>
-        <ul className={`nav__list ${isOpen && 'nav__list--active'}`}>
+        <ul className={`nav__list ${isOpen && "nav__list--active"}`}>
           <li className="nav__item">
-            <a href="#1" className="nav__link c-blue">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg>
-            </a>
+            <NavLink to="/" className="nav__link c-blue">
+              <HomeIcon
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-archive"
+              />
+            </NavLink>
           </li>
           <li className="nav__item">
-            <a href="#2" className="nav__link c-yellow scrolly">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg>
-            </a>
+            <NavLink
+              to="/client/reservations"
+              className="nav__link c-yellow scrolly"
+            >
+              <CalendarIcon
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-archive"
+              />
+            </NavLink>
           </li>
           <li className="nav__item">
-            <a href="#3" className="nav__link c-red">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg>
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#4" className="nav__link c-green">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg>
-            </a>
+            <div
+              className="nav__link c-red cursor-pointer"
+              onClick={() => {
+                logout() && navigate("/login");
+              }}
+            >
+              <LogoutIcon
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-archive"
+              />
+            </div>
           </li>
         </ul>
       </nav>

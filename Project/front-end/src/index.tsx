ea63d9892df639d238f230c3f4b7d0e0bb26342f
Mouhcine Daali <daalim277@gmail.com>
2022-06-20 23:48:05 +0100

    use the router in index component

--- Project/front-end/src/index.tsx
+++ Project/front-end/src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './components/app/app.component';
-
+import App from "./components/app/app.component";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
-

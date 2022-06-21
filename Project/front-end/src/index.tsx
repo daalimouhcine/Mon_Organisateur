cca4fa00afb6fee8cf8aee1c893d57ef2d77db31
Mouhcine Daali <daalim277@gmail.com>
2022-06-21 23:56:25 +0100

    use store

--- Project/front-end/src/index.tsx
+++ Project/front-end/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/app/app.component";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );

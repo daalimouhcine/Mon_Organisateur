0306d92ea843e5564a568dd9d98f3b80abdce34c
Mouhcine Daali <daalim277@gmail.com>
2022-06-21 04:07:36 +0100

    compleet design

---
+++ Project/front-end/src/components/organisateur/loading.component.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./loading.component.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loadingContainer fixed w-full h-screen top-0 left-0 z-50 bg-[#BA9672] opacity-20 flex justify-center justify-items-center align-middle">
+      <div className="loader loader4">
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        <div>
+                          <div></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;

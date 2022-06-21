14adc67c4d9177b589084764d5e221248c5ef7d0
Mouhcine Daali <daalim277@gmail.com>
2022-06-21 23:58:05 +0100

    use redux

--- Project/front-end/src/slices/profile.ts
+++ Project/front-end/src/slices/profile.ts
@@ -1,8 +1,28 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+interface ProfileState {
+  user: any;
+  isAuthed: boolean;
+}
 
 const profileSlice = createSlice({
-    name: 'profile',
-    initialState: 'initialState',
-    reducers: {},
-  });
+  name: "profile",
+  initialState: {
+    user: {},
+    isAuthed: false,
+  } as ProfileState,
+  reducers: {
+    setUser: (state: ProfileState, action: PayloadAction<any>) => {
+      state.user = action.payload;
+      state.isAuthed = true;
+    },
+    setIsAuthed: (state: ProfileState, action: PayloadAction<any>) => {},
+    logOut: (state: ProfileState) => {
+      state.user = {};
+      state.isAuthed = false;
+    },
+  },
+});
+export const { setIsAuthed, setUser, logOut } = profileSlice.actions;
+
+export const profileReducer = profileSlice.reducer;

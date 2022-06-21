f6aad1c4082668aa54a20f4a144de290f7d173d1
Mouhcine Daali <daalim277@gmail.com>
2022-06-21 23:58:41 +0100

    delete unsed typing

--- Project/front-end/src/pages/auth/login/login.page.tsx
+++ Project/front-end/src/pages/auth/login/login.page.tsx
@@ -2,9 +2,12 @@ import LoginForm from "../../../components/login/login_form.component";
 
 import "./login.page.css";
 
-const Login: React.FC = () => {
+//@ts-ignore
+const Login: React.FC = ({isAuthed}) => {
   return (
     <section className="h-screen flex pattern-cross-dots-md bg-[#e7bf97] text-[#100D3F]">
+      
+      {isAuthed}
       <LoginForm />
     </section>
   );
